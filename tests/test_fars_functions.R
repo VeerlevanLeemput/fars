41aeb94c96fd1f37b99d75029629725cd2488ea1
Veerle van Leemput <veerlevanleemput@live.nl>
2017-12-20 07:59:28 +0100

    Changed test file

--- tests/test_fars_functions.R
+++ tests/test_fars_functions.R
@@ -1,6 +1,5 @@
 ## This file contains a test for the fars package
 
-source("R/fars_functions.R")
 library(testthat)
 
 test_that("Filename is a character string", expect_that(make_filename(2014), is_a("character")))
